6e19afd3f33ee2465942ef9da1187eef869f0cd8
Alef Carvalho <102701451+acarvalhooo@users.noreply.github.com>
2023-06-27 11:18:35 -0300

    Update SumBucketsSizes.py

--- SumBucketsSizes/SumBucketsSizes.py
+++ SumBucketsSizes/SumBucketsSizes.py
@@ -1,9 +1,3 @@
-# This code was created to use Python 3.9
-
-# If you need fast answers about the size of all your buckets and wait 24 hours for metrics in Storage Lens isn't viable, you can use the code below.
-# However, if the lambda function exceeds the limit of 15 minutes, you should consider use pagination and threads.
-# Even if this isn't enough, consider a container usage to monitoring.
-
 # Importing boto3 library
 import boto3
 
